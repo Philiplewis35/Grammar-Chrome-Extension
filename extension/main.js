72a2ed2dce37c615adef9032aca703f01d28d975
Philiplewis35 <philiplewis35@gmail.com>
2019-12-11 15:48:51 +0000

    only check grammar once user has stopped typing for three seconds

--- extension/main.js
+++ extension/main.js
@@ -1,62 +1,41 @@
+var last_event;
 $( document ).ready(function() {
-  google_doc_obsever()
+  observe_google_doc();
 });
 
-var pause = false;
 
-function addStyleString(str) {
-  var node = document.createElement('style');
-  node.innerHTML = str;
-  document.body.appendChild(node);
-}
-
-function google_doc_obsever() {
+function observe_google_doc() {
   var typingTimer;
+  var doneTypingInterval = 3000;
+  var input = $('iframe.docs-texteventtarget-iframe')[0];
+  input.contentDocument.addEventListener("keydown", clearTimeout(typingTimer), false);
+  input.contentDocument.addEventListener("keyup", function() {
+    clearTimeout(typingTimer);
+    typingTimer = setTimeout(doneTyping, doneTypingInterval);
+  }, false);
 
-  if($('.docs-gm')[0]){
-    const env = new Mark($('.kix-paragraphrenderer')[0]);
+  const set_event = function(events) {
+    window.last_event = events[0]
+  };
+  const observer = new MutationObserver(set_event);
+  observer.observe($('.kix-paragraphrenderer')[0], { attributes: true, subtree: true }); // TODO: All paragraphs
+};
 
-
-    const callback = function(events) {
-      if(pause == true){ return }
-      pause = true
-      collectText(env, events[0])
-    };
-
-    const observer = new MutationObserver(callback);
-    observer.observe($('.kix-paragraphrenderer')[0], { attributes: true, subtree: true }); // TODO: All paragraphs
-  }
+function doneTyping() {
+  collectText(window.last_event)
 }
 
-function collectText(env, event) {
-  var paragraph = jQuery(event.target).parents('.kix-paragraphrenderer').first()
+function collectText(typing_event) {
+  var paragraph = jQuery(typing_event.target).parents('.kix-paragraphrenderer').first()
   var text = ""
+
   $.each(paragraph.children(), function(index, line) {
     line = jQuery(line).find('.kix-wordhtmlgenerator-word-node')
     text += line.text()
   })
-  send_to_api(env, text);
-}
 
-function send_to_api(env, text) {
   chrome.runtime.sendMessage({purpose: "check grammar", data: text}, function(response) {
     var sentences =  Object.keys(response)
-    highlight_text(env, sentences)
+    highlight_text(paragraph[0], sentences)
   });
-};
-
-function highlight_text(context, sentences) {
-  $.each(sentences, function(index, sentence) {
-    phrase = sentence.split(' ').join('.*'); // double spaced?
-    context.markRegExp(new RegExp(phrase), {"className": 'grammar'})
-    pause = false;
-
-    $('.grammar').append("<div class='box' <p>this is hidden</p></div>");
-    addStyleString('.box { color: white; background-color: green; width: 100px; height: 100px; z-index: 100;}')
-    // $('.grammar').hover(function() {
-    //   $('.box').css('display', 'block')
-    // }, function() {
-    //   $('.box').css('display', 'none')
-    // })
-  })
 }
