9d61a2e12d0ea08a138f9ae7234bb2ce1824e0ed
Philiplewis35 <philiplewis35@gmail.com>
2019-12-22 19:17:09 +0000

    post to heroku

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
  if (message.purpose === 'check grammar'){
-   $.post('http://www.philip35.com', message.data, function(r){sendResponse(r)})
+   $.post('https://parser3.herokuapp.com/', message.data, function(r){sendResponse(r)})
    return true;
  };
 });
