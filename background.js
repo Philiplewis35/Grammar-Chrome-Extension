9a3b5cc37f87a36c5603e0106c432a1f62bd9931
Philiplewis35 <philiplewis35@gmail.com>
2019-12-24 11:01:41 +0000

    stop using regex when marking text

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
  if (message.purpose === 'check grammar'){
-   $.post('https://parser3.herokuapp.com/', message.data, function(r){sendResponse(r)})
+   console.log(message.data)
+   $.post('http://localhost:4567/', message.data, function(r){sendResponse(r)})
    return true;
  };
 });
--- interface.js
+++ interface.js
@@ -1,7 +1,9 @@
 $( document ).ready(function() {
-  addStyleString('.kix-lineview-content {position: static !important; }')
-  addStyleString('.box { display: none; background-color: white; color: dark-grey; height: 75px; position: absolute; margin-top: 0.75em; padding: 1em 1em 1em 1em; z-index: 20; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);}')
-  addStyleString(".box:after { content: ''; position: absolute; top: 0; left: 50%; width: 0; height: 0; border: 0.625em solid transparent; border-bottom-color: white; border-top: 0; margin-left: -0.625em; margin-top: -0.625em;}")
+  if($(".kix-lineview-content")[0]) {
+    addStyleString('.kix-lineview-content {position: static !important; }')
+    addStyleString('.box { display: none; background-color: white; color: dark-grey; height: 75px; position: absolute; margin-top: 0.75em; padding: 1em 1em 1em 1em; z-index: 20; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);}')
+    addStyleString(".box:after { content: ''; position: absolute; top: 0; left: 50%; width: 0; height: 0; border: 0.625em solid transparent; border-bottom-color: white; border-top: 0; margin-left: -0.625em; margin-top: -0.625em;}")
+  }
 });
 
 function addStyleString(str) {
@@ -15,8 +17,8 @@ function highlight_text(context, sentences) {
   $.each(sentences, function(sentence, response) {
     suggestion_id = response[0]
     suggestion = response[1]
-    phrase = sentence.split(' ').join('.*'); // double spaced?
-    instance.markRegExp(new RegExp(phrase), {"className": 'grammar', "exclude": ['.box'], "acrossElements": true, "each": function(node) {
+    instance.mark(sentence, {className: 'grammar', separateWordSearch: false, ignoreJoiners: true,
+    exclude: ['.box'], acrossElements: true, each: function(node) {
       $(node).removeClass('box')
       $(node).append("<div class='box' id=" + suggestion_id + ">" + suggestion + "</div>");
       suggestion_box = $("#" + suggestion_id + ".box")[0]
