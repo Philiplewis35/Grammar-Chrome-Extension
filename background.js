dc23d26f7637982cf128b327c74db90bbb9cdbf6
Philiplewis35 <philiplewis35@gmail.com>
2020-02-11 12:02:51 +0000

    listen to external messages

--- background.js
+++ background.js
@@ -2,13 +2,27 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   local = 'http://localhost:4567/'
   heroku = 'https://parser3.herokuapp.com'
 
- if (message.purpose === 'check grammar'){
-   $.post(local, message.data, function(r){sendResponse(r)})
+  url = local
+
+ if (message.purpose && message.purpose === 'check grammar'){
+   $.post(url, message.data, function(r){sendResponse(r)})
    return true;
  };
 
- if(message.purpose === 'ignore') {
-   $.post(local + 'ignore', message.data, function(r){sendResponse(r)})
+ if(message.purpose && message.purpose === 'ignore') {
+   $.post(url + 'ignore', message.data, function(r){sendResponse(r)})
+   return true;
+ };
+
+ if(message.purpose && message.purpose === 'explain') {
+   $.get(url + 'explainer', message.data, function(r){sendResponse(r)})
    return true;
  };
 });
+
+chrome.runtime.onMessageExternal.addListener(
+  function(request, sender, sendResponse) {
+    console.log(request)
+    console.log(sender)
+    console.log(sendResponse)
+  });
