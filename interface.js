70060800b11622580dd4fa6dccd937f2c684a41f
Philiplewis35 <philiplewis35@gmail.com>
2020-02-13 12:22:25 +0000

    refactor marking loop and boxes

--- interface.js
+++ interface.js
@@ -59,44 +59,45 @@ function mark_hover() { // opacity 0 = hidden
   })
 }
 
-function highlight_text(context, sentences) {
+function highlight_text(context, responses) {
   var instance = new Mark(context);
-  $.each(sentences, function(sentence, response) {
-    suggestion_id = response[0]
-    suggestion = response[1]
-    marks[suggestion_id] = instance
-    console.log(sentence)
-    instance.mark(sentence, {className: 'grammar', separateWordSearch: false, ignoreJoiners: true,
-    exclude: ['.box'], acrossElements: true, each: function(node) {
 
-      if(node.childNodes[0].nodeValue.replace(/[^\x00-\x7F]/g, "").length > 0) {
-        $(node).append("<div class='box' id=box_" + suggestion_id + ">" + suggestion + "<div class='options_border'></div><button type='button' class='btn btn-primary btn-sm ignore_text'>Ignore</button></div>");
-        suggestion_box = $(node).find('.box')[0]
-        page = $('.kix-page-column')[0]
+  if(responses.length > 0) {
+    $.each(responses, function(response_index, response) {
+      instance.mark(response.phrase, {className: 'grammar', separateWordSearch: false, ignoreJoiners: true,
+      exclude: ['.box'], acrossElements: true, each: function(node) {
 
-        mark_width = node.getBoundingClientRect().width
-        mark_center = node.getBoundingClientRect().x + (mark_width / 2)
-        mark_right = node.getBoundingClientRect().x + mark_width
+        if(node.childNodes[0].nodeValue.replace(/[^\x00-\x7F]/g, "").length > 0) {
+          $(node).append("<div class='box' id=box_" + response.id + ">" + response.explanation + "<div class='options_border'></div></div>");
+          suggestion_box = $(node).find('.box')[0]
+          page = $('.kix-page-column')[0]
 
-        box_left = suggestion_box.getBoundingClientRect().x
-        box_width = suggestion_box.getBoundingClientRect().width
-        box_center = suggestion_box.getBoundingClientRect().x + (box_width / 2)
-        box_right = suggestion_box.getBoundingClientRect().x + box_width
+          mark_width = node.getBoundingClientRect().width
+          mark_center = node.getBoundingClientRect().x + (mark_width / 2)
+          mark_right = node.getBoundingClientRect().x + mark_width
 
-        page_right = page.getBoundingClientRect().x + page.getBoundingClientRect().width
-
-        if(mark_right > box_right) {
-          // align box center with center of phrase
-          box_padding = (mark_center - box_center)
-          suggestion_box.style.marginLeft = box_padding + 'px';
+          box_left = suggestion_box.getBoundingClientRect().x
+          box_width = suggestion_box.getBoundingClientRect().width
+          box_center = suggestion_box.getBoundingClientRect().x + (box_width / 2)
           box_right = suggestion_box.getBoundingClientRect().x + box_width
-        }
 
-        if(box_right > (page_right - 20)) { // if box has gone off right edge
-          box_padding = ((page_right - box_left) - (box_width + 20))
-          suggestion_box.style.marginLeft = box_padding + 'px';
+          page_right = page.getBoundingClientRect().x + page.getBoundingClientRect().width
+
+          if(mark_right > box_right) {
+            // align box center with center of phrase
+            box_padding = (mark_center - box_center)
+            suggestion_box.style.marginLeft = box_padding + 'px';
+            box_right = suggestion_box.getBoundingClientRect().x + box_width
+          }
+
+          if(box_right > (page_right - 20)) { // if box has gone off right edge
+            box_padding = ((page_right - box_left) - (box_width + 20))
+            suggestion_box.style.marginLeft = box_padding + 'px';
+          }
         }
-      }
-    }})
-  })
+      }})
+    })
+  } else {
+    console.log('no passive')
+  }
 }
