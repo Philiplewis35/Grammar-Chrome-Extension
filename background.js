4f99c6352747f19b13c28b343d90f2dc88d21d32
Philiplewis35 <philiplewis35@gmail.com>
2020-02-22 11:24:02 +0000

    use callbacks properly

--- background.js
+++ background.js
@@ -1,14 +1,11 @@
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
  if (message.purpose && message.purpose === 'check grammar'){
-   chrome.storage.sync.get(['grammar_services'], function(results) {
-     responses = []
+   responses = []
 
-     production = results.grammar_services.services
-     local = ['http://localhost:4567/analyse', 'http://localhost:5678/analyse']
-
-     $.each(local, function(index, service_url) {
+   chrome.storage.sync.get(['gc_services'], function(r){
+     $.each(r['gc_services'], function(index, service_url) {
        $.ajax({
-         url: service_url,
+         url: service_url + '/analyse',
          method: 'POST',
          data: message.data,
          async: false,
@@ -18,15 +15,9 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
        })
 
      });
-
+     console.log(responses)
      sendResponse(responses)
-   });
+   })
    return true
  };
 });
-
-chrome.runtime.onMessageExternal.addListener(
-  function(request, sender, sendResponse) {
-    chrome.storage.sync.set({grammar_services: request}, function() {});
-  }
-);
