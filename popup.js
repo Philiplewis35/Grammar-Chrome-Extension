1ca37a2b448d5281ba6ecebc1506ab268fd51e44
Philiplewis35 <philiplewis35@gmail.com>
2020-03-12 17:58:02 +0000

    use more custom class names

--- popup.js
+++ popup.js
@@ -22,11 +22,11 @@ function initialise_popup() {
 }
 
 function render_log_in_page() {
-  $('body').html('<h3>Log in to Grammar checker</h3><div class="form">  <div class="field email">    <label for="user_email">Email</label><br>    <input autofocus="autofocus" autocomplete="email" type="email" value="" name="user[email]" id="user_email">  </div>  <div class="field password">    <label for="user_password">Password</label><br>    <input autocomplete="current-password" type="password" name="user[password]" id="user_password">  </div>  <div class="field">    <input name="user[remember_me]" type="hidden" value="0"><input type="checkbox" value="1" name="user[remember_me]" id="user_remember_me">    <label for="user_remember_me">Remember me</label>  </div>  <div class="actions">    <input type="submit" name="commit" value="Log in" data-disable-with="Log in" class="submit_log_in">  </div></div>')
+  $('body').html('<h3>Log in to Grammar checker</h3><div class="form"><div class="field email"><label for="user_email">Email</label><br><input autofocus="autofocus" autocomplete="email" type="email" value="" name="user[email]" id="user_email">  </div>  <div class="field password">    <label for="user_password">Password</label><br>    <input autocomplete="current-password" type="password" name="user[password]" id="user_password">  </div>  <div class="field">    <input name="user[remember_me]" type="hidden" value="0"><input type="checkbox" value="1" name="user[remember_me]" id="user_remember_me">    <label for="user_remember_me">Remember me</label>  </div>  <div class="actions">    <input type="submit" name="commit" value="Log in" data-disable-with="Log in" class="gc_submit_log_in">  </div></div>')
 }
 
 function render_signed_in_page() {
-  $('body').html('<a href="https://grammar-checker1.herokuapp.com/" target="_blank">Go to grammar checker</a><input type="submit" name="commit" value="Log out" class="log_out">')
+  $('body').html('<a href="https://grammar-checker1.herokuapp.com/" target="_blank">Go to grammar checker</a></br><input type="submit" name="commit" value="Log out" class="gc_log_out">')
 }
 
 function get_services() {
@@ -50,7 +50,7 @@ function get_services() {
 //listeners
 
 // log in
-$(document).on('click', '.submit_log_in', function(){
+$(document).on('click', '.gc_submit_log_in', function(){
   email = $('#user_email').val()
   password = $('#user_password').val()
 
@@ -60,23 +60,23 @@ $(document).on('click', '.submit_log_in', function(){
     data: {email: email, password: password},
     success: function(response) { // sign in
       render_signed_in_page()
-      $('body').prepend('<p class="success">Signed In</p>')
+      $('body').prepend('<p class="gc_success_tet">Signed In</p>')
       chrome.storage.local.set({gc_email: response['email']}, function(){});
       chrome.storage.local.set({gc_auth: response['authentication_token']}, function(){});
       get_services() // syncs services to chrome storage
     },
     error: function() { // show error message on log in page
-      if($('.error')[0]) {
-        $('.error').html('Invalid username or password')
+      if($('.gc_error_text')[0]) {
+        $('.gc_error_text').html('Invalid username or password')
       } else {
-        $('body').prepend('<p class="error">Invalid username or password</p>')
+        $('body').prepend('<p class="gc_error_text">Invalid username or password</p>')
       }
     }
   });
 })
 
 // log out
-$(document).on('click', '.log_out', function(){
+$(document).on('click', '.gc_log_out', function(){
   chrome.storage.local.get(['gc_email', 'gc_auth'], function(headers) {
     $.ajax({
       url: "https://grammar-checker1.herokuapp.com/api/v1/sign_out",
@@ -89,10 +89,10 @@ $(document).on('click', '.log_out', function(){
         render_log_in_page()
       },
       error: function() {
-        if($('.error')[0]) {
-          $('.error').html('Could not log out')
+        if($('.gc_error_text')[0]) {
+          $('.gc_error_text').html('Could not log out')
         } else {
-          $('body').prepend('<p class="error">Could not log out</p>')
+          $('body').prepend('<p class="gc_error_text">Could not log out</p>')
         }
       }
     })
