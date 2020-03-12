fa3f2b3312096b2db293ac7ddd423a18c160dcc7
Philiplewis35 <philiplewis35@gmail.com>
2020-03-12 17:59:25 +0000

    custom vars and copy suggestion

--- interface.js
+++ interface.js
@@ -1,24 +1,25 @@
-$(document).on('mouseover', '.grammar', function() {$(this).find('.box').css('display', 'block')})
-$(document).on('mouseover', '.box', function(event) {event.stopPropagation()})
-$(document).on('click', '.close_box', function() {$(this).closest('.box').css('display', 'none')})
+$(document).on('mouseover', '.grammar', function() {$(this).find('.gc_box').css('display', 'block')})
+$(document).on('mouseover', '.gc_box', function(event) {event.stopPropagation()})
+$(document).on('click', '.close_gc_box', function() {$(this).closest('.gc_box').css('display', 'none')})
 $(document).on('mouseover', '.suggestion', function() { $(this).addClass('btn-primary'); })
 $(document).on('mouseout', '.suggestion', function() { $(this).removeClass('btn-primary'); })
 
 $(document).on('click', '.ignore_text', function(e) {
   $(this).closest('.grammar').css('border-bottom', 'none')
-  box = $(this).closest('.box')
-  box.remove()
+  gc_box = $(this).closest('.gc_box')
+  gc_box.remove()
 })
 
 $(document).on('click', '.suggestion', function(e) {
-  // underlined = $(this).closest('.grammar')[0]
-  // underlined.childNodes[0].nodeValue = $(this)[0].textContent
-  // underlined.remove()
-  // $(this).closest('.grammar').css('border-bottom', 'none')
-  // box = $(this).closest('.box')
-  // box.remove()
+  text = document.createElement('textarea')
+  text.value = $(this)[0].textContent
+  document.body.appendChild(text)
+  text.select();
+  document.execCommand('copy')
+  document.body.removeChild(text)
+  gc_box = $(this).closest('.gc_box')
+  gc_box.remove()
 })
-
 
 function highlight_text(context, responses) {
   if(responses.length > 0) {
@@ -28,36 +29,36 @@ function highlight_text(context, responses) {
           if(line.childNodes[0].nodeValue.replace(/[^\x00-\x7F]/g, "").search(response.phrase) >= 0) {
             $(line).html($(line).html().replace(/[^\x00-\x7F]/g, "").replace(response.phrase, '<span class=grammar>' + response.phrase + '</span'))
             node = $(line).find('.grammar')[0]
-            $(node).append("<div class='box'>" + '<div class="card"><div class="card-header">' + response.explanation + '</div><ul class="list-group list-group-flush"><li class="list-group-item suggestion">' + response.suggested_replacement + '<i class="far fa-copy"></i></li><li class="list-group-item"><div class= "ignore_text btn btn-primary btn-sm">Ignore</div><div class="close_box btn btn-danger btn-sm">Close</div></li></ul></div></div>');
-            suggestion_box = $(node).find('.box')[0]
+            $(node).append("<div class='gc_box'>" + '<div class="card"><div class="card-header">' + response.explanation + '</div><ul class="list-group list-group-flush"><li class="list-group-item suggestion" title="copy suggestion">' + response.suggested_replacement + '<i class="far fa-copy"></i></li><li class="list-group-item"><div class= "ignore_text btn btn-primary btn-sm">Ignore</div><div class="close_gc_box btn btn-danger btn-sm">Close</div></li></ul></div></div>');
+            suggestion_gc_box = $(node).find('.gc_box')[0]
 
             mark_width = node.getBoundingClientRect().width
             mark_center = node.getBoundingClientRect().x + (mark_width / 2)
             mark_right = node.getBoundingClientRect().x + mark_width
 
-            box_left = suggestion_box.getBoundingClientRect().x
-            box_width = suggestion_box.getBoundingClientRect().width
-            box_center = suggestion_box.getBoundingClientRect().x + (box_width / 2)
-            box_right = suggestion_box.getBoundingClientRect().x + box_width
+            gc_box_left = suggestion_gc_box.getBoundingClientRect().x
+            gc_box_width = suggestion_gc_box.getBoundingClientRect().width
+            gc_box_center = suggestion_gc_box.getBoundingClientRect().x + (gc_box_width / 2)
+            gc_box_right = suggestion_gc_box.getBoundingClientRect().x + gc_box_width
 
             page = $('.kix-page-column')[0]
             page_right = page.getBoundingClientRect().x + page.getBoundingClientRect().width
 
-            if(mark_right > box_right) {
-              // align box center with center of phrase
-              box_padding = (mark_center - box_center)
-              suggestion_box.style.marginLeft = box_padding + 'px';
-              box_right = suggestion_box.getBoundingClientRect().x + box_width
+            if(mark_right > gc_box_right) {
+              // align gc_box center with center of phrase
+              gc_box_padding = (mark_center - gc_box_center)
+              suggestion_gc_box.style.marginLeft = gc_box_padding + 'px';
+              gc_box_right = suggestion_gc_box.getBoundingClientRect().x + gc_box_width
             }
 
-            if(box_right > (page_right - 20)) { // if box has gone off right edge
-              box_padding = ((page_right - box_left) - (box_width + 20))
-              suggestion_box.style.marginLeft = box_padding + 'px';
+            if(gc_box_right > (page_right - 20)) { // if gc_box has gone off right edge
+              gc_box_padding = ((page_right - gc_box_left) - (gc_box_width + 20))
+              suggestion_gc_box.style.marginLeft = gc_box_padding + 'px';
             }
 
-            // change from opacity 0 to display none now box has been generated
-            $(suggestion_box).css('display', 'none')
-            $(suggestion_box).css('opacity', '1')
+            // change from opacity 0 to display none now gc_box has been generated
+            $(suggestion_gc_box).css('display', 'none')
+            $(suggestion_gc_box).css('opacity', '1')
           }
         });
       })
