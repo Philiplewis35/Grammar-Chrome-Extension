5339fed6efee1de3281ffc53ad1d56b52de2dde0
Philiplewis35 <philiplewis35@gmail.com>
2020-04-06 13:05:39 +0100

    re add mark js + copy suggestion

--- interface.js
+++ interface.js
@@ -1,53 +1,88 @@
-$(document).on('mouseover', '.grammar', function() {$(this).find('.gc_box').css('display', 'block')})
-$(document).on('mouseover', '.gc_box', function(event) {event.stopPropagation()})
-$(document).on('click', '.close_gc_box', function() {$(this).closest('.gc_box').css('display', 'none')})
+window.instance = new Mark('.kix-paragraphrenderer')
 
-$(document).on('click', '.ignore_text', function(e) {
-  $(this).closest('.grammar').css('border-bottom', 'none')
-  gc_box = $(this).closest('.gc_box')
-  gc_box.remove()
+$(document).on('mouseover', '.grammar', function() {
+  $.each($('.gc_box'), function(index, suggestion_box) {
+    $(this).css('display', 'none')
+  })
+  $(this).find('.gc_box').css('display', 'block')
 })
 
-function highlight_text(context, responses) {
+$(document).on('mouseover', '.gc_box', function(event) {event.stopPropagation()})
+$(document).on('click', '.close_gc_box', function() {$(this).closest('.gc_box').css('display', 'none')})
+$(document).on('mouseover', '.copy_suggestion', function() { $(this).addClass('btn-primary') })
+$(document).on('mouseout', '.copy_suggestion', function() { $(this).removeClass('btn-primary') })
+
+$(document).on('click', '.ignore_text', function(e) {
+  paragraph = $(this).closest('.kix-paragraphrenderer')
+  mark = $(this).closest('mark')[0]
+  id = mark.classList[mark.classList.length - 1]
+  $('.' + id + ' > .gc_box').remove();
+  window.instance.unmark({className: id})
+})
+
+$(document).on('click', '.copy_suggestion', function() {
+  str = this.innerText
+  el = document.createElement('textarea');
+  el.value = str;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+  $(this).closest('.gc_box').css('display', 'none')
+  $('body').prepend('<div class = "gc_alert_box">suggestion coppied to clipboard</div>')
+  setTimeout(function(){ $('.gc_alert_box').remove() }, 3000);
+})
+
+
+function highlight_text(responses) {
   if(responses.length > 0) {
     $.each(responses, function(services_index, services) {
       $.each(services, function(response_index, response) {
-        $.each($(context).find('.kix-wordhtmlgenerator-word-node'), function(index, line) {
-          if(line.childNodes[0].nodeValue.replace(/[^\x00-\x7F]/g, "").search(response.phrase) >= 0) {
-            $(line).html($(line).html().replace(/[^\x00-\x7F]/g, "").replace(response.phrase, '<span class=grammar>' + response.phrase + '</span'))
-            node = $(line).find('.grammar')[0]
-            $(node).append("<div class='gc_box'>" + '<div class="card"><div class="card-header">' + response.explanation + '</div><ul class="list-group list-group-flush"><li class="list-group-item suggestion">' + response.suggested_replacement + '</li><li class="list-group-item"><div class= "ignore_text btn btn-primary btn-sm">Ignore</div><div class="close_gc_box btn btn-danger btn-sm">Close</div></li></ul></div></div>');
-            suggestion_gc_box = $(node).find('.gc_box')[0]
 
-            mark_width = node.getBoundingClientRect().width
-            mark_center = node.getBoundingClientRect().x + (mark_width / 2)
-            mark_right = node.getBoundingClientRect().x + mark_width
+        window.instance.mark(response.phrase, { className: 'grammar', acrossElements: true, ignoreJoiners: true, separateWordSearch: false, exclude: ['mark'], filter: function(text_node, term, all_marks_no, term_marks_no) {
+          window.marks_no = term_marks_no
+          return true
+        }, each: function(node) {
 
-            gc_box_left = suggestion_gc_box.getBoundingClientRect().x
-            gc_box_width = suggestion_gc_box.getBoundingClientRect().width
-            gc_box_center = suggestion_gc_box.getBoundingClientRect().x + (gc_box_width / 2)
-            gc_box_right = suggestion_gc_box.getBoundingClientRect().x + gc_box_width
+          grammar_class =  'grammar_' + window.response_id + '_' + window.marks_no
+          $(node).addClass(grammar_class)
 
-            page = $('.kix-page-column')[0]
-            page_right = page.getBoundingClientRect().x + page.getBoundingClientRect().width
 
-            if(mark_right > gc_box_right) {
-              // align gc_box center with center of phrase
-              gc_box_padding = (mark_center - gc_box_center)
-              suggestion_gc_box.style.marginLeft = gc_box_padding + 'px';
-              gc_box_right = suggestion_gc_box.getBoundingClientRect().x + gc_box_width
-            }
-
-            if(gc_box_right > (page_right - 20)) { // if gc_box has gone off right edge
-              gc_box_padding = ((page_right - gc_box_left) - (gc_box_width + 20))
-              suggestion_gc_box.style.marginLeft = gc_box_padding + 'px';
-            }
-
-            // change from opacity 0 to display none now gc_box has been generated
-            $(suggestion_gc_box).css('display', 'none')
-            $(suggestion_gc_box).css('opacity', '1')
+          $(node).append("<div class='gc_box'>" + '<div class="card"><div class="card-header">' + response.explanation + '</div><ul class="list-group list-group-flush"><li class="list-group-item suggestion">' + response.suggestion + '</li><li class="list-group-item"><div class= "ignore_text btn btn-primary btn-sm">Ignore</div><div class="close_gc_box btn btn-danger btn-sm">Close</div></li></ul></div></div>');
+          if(response.substituion == true) {
+            $(node).find('.suggestion').addClass('copy_suggestion')
           }
-        });
+
+          suggestion_gc_box = $(node).find('.gc_box')[0]
+
+          mark_width = node.getBoundingClientRect().width
+          mark_center = node.getBoundingClientRect().x + (mark_width / 2)
+          mark_right = node.getBoundingClientRect().x + mark_width
+
+          gc_box_left = suggestion_gc_box.getBoundingClientRect().x
+          gc_box_width = suggestion_gc_box.getBoundingClientRect().width
+          gc_box_center = suggestion_gc_box.getBoundingClientRect().x + (gc_box_width / 2)
+          gc_box_right = suggestion_gc_box.getBoundingClientRect().x + gc_box_width
+
+          page = $('.kix-page-column')[0]
+          page_right = page.getBoundingClientRect().x + page.getBoundingClientRect().width
+
+          if(mark_right > gc_box_right) {
+            // align gc_box center with center of phrase
+            gc_box_padding = (mark_center - gc_box_center)
+            suggestion_gc_box.style.marginLeft = gc_box_padding + 'px';
+            gc_box_right = suggestion_gc_box.getBoundingClientRect().x + gc_box_width
+          }
+
+          if(gc_box_right > (page_right - 20)) { // if gc_box has gone off right edge
+            gc_box_padding = ((page_right - gc_box_left) - (gc_box_width + 20))
+            suggestion_gc_box.style.marginLeft = gc_box_padding + 'px';
+          }
+
+          // change from opacity 0 to display none now gc_box has been generated
+          $(suggestion_gc_box).css('display', 'none')
+          $(suggestion_gc_box).css('opacity', '1')
+        }})
       })
     })
   }
